affe61fbabca4f43b0775801b7fa1e34577285c8
afifi brahim <afifi71brahim@gmail.com>
2023-01-29 18:18:56 +0100

    change the color

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -21,11 +21,10 @@ align-items: center;
 margin-top: 1rem;
 .icon{
   font-size: 1.7rem;
-  background:linear-gradient(to left,#fc4a1a,#f7b733);
+  /* background:linear-gradient(to left,#fc4a1a,#f7b733);
   -webkit-text-fill-color: transparent;
-  background-clip:text;
+  background-clip:text; */
   color: red;
-
 }
 p{
   font-size: 1rem;
@@ -35,9 +34,17 @@ p{
   align-items: center;
 }
 .active{
-  background:linear-gradient(to left,#fc4a1a,#f7b733);
+  /* background:linear-gradient(to left,#fc4a1a,#f7b733);
   -webkit-text-fill-color: transparent;
-  background-clip:text;
+  background-clip:text; */
+  background: linear-gradient(to left, #fc4a1a, #f7b733);
+background: -webkit-linear-gradient(to left, #fc4a1a, #f7b733);
+background: -moz-linear-gradient(to left, #fc4a1a, #f7b733);
+background: -o-linear-gradient(to left, #fc4a1a, #f7b733);
+-webkit-background-clip: text;
+background-clip: text;
+-webkit-text-fill-color: transparent;
+
 
 }
 `
